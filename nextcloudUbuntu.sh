d13a73dce8f868f3f61d00a50e178e8055efedbc
ani-6 <animahajan6@gmail.com>
2022-05-29 20:14:10 +0530

    Changed coding

--- nextcloudUbuntu.sh
+++ nextcloudUbuntu.sh
@@ -1,73 +1,112 @@
 #!/bin/sh
+# Color Reset
+Color_Off='\033[0m'       # Reset
 
-sudo apt update
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
 
-sudo apt install -y php imagemagick php-imagick libapache2-mod-php7.4 php7.4-common php7.4-mysql php7.4-fpm php7.4-gd php7.4-json php7.4-curl php7.4-zip php7.4-xml php7.4-mbstring php7.4-bz2 php7.4-intl php7.4-bcmath php7.4-gmp
+update() {
+	# Update system repos
+	echo -e "\n ${Cyan} Updating package repositories.. ${Color_Off}"
+	sudo apt -qq update
+}
 
-sudo systemctl reload apache2
+installphpext(){
+    echo -e "\n ${Cyan} Installing php extensions.. ${Color_Off}"
+    sudo apt install -y php imagemagick php-imagick libapache2-mod-php php-common php-mysql php-fpm php-gd php-json php-curl php-zip php-xml php-mbstring php-bz2 php-intl php-bcmath php-gmp
+    sudo systemctl reload apache2
+} &> /dev/null
 
-latestVer=$(curl -s https://github.com/nextcloud/server/releases |
-    grep -m1 -Eo "[^/]+\.zip")
+getlatestnextcloud(){
+    latestVer=$(curl -s https://github.com/nextcloud/server/releases |
+        grep -m1 -Eo "[^/]+\.zip")
 
-Ver="${latestVer:1}"
+    Ver="${latestVer:1}"
 
-echo "Installing Nextcloud Stable version - ${Ver}"
+    echo -e"\n ${Green} Installing Nextcloud Stable version - ${Ver}"
 
-sudo rm -rf nextcloud-${Ver}
-# Download latest version:
-wget "https://download.nextcloud.com/server/releases/nextcloud-${Ver}"
+    sudo rm -rf nextcloud-${Ver}
+    # Download latest version:
+    wget "https://download.nextcloud.com/server/releases/nextcloud-${Ver}"
 
-unzip nextcloud-${Ver}
-
-sudo mv nextcloud /var/www/html/
+    unzip nextcloud-${Ver}
+    sudo mv nextcloud /var/www/html/
+}
 
 #sudo mv nextcloud html
-sudo chown www-data:www-data /var/www/html/nextcloud/ -R
+setPermissions(){
+    echo -e "\n ${Cyan} Setting permissions for nextcloud folder.. ${Color_Off}"
+    sudo chown www-data:www-data /var/www/html/nextcloud/ -R
+}
 
-echo "Creating virtual host"
+VirtualHost(){
+    echo "Creating virtual host"
 
-sudo sh -c '
-echo "<VirtualHost *:80>
-    ServerAdmin admin@example.com
-    DocumentRoot "/var/www/html/nextcloud"
-    ServerName pcloud.com
-    ServerAlias www.pcloud.com
+    sudo sh -c '
+    echo "<VirtualHost *:80>
+        ServerAdmin admin@example.com
+        DocumentRoot "/var/www/html/nextcloud"
+        ServerName pcloud.com
+        ServerAlias www.pcloud.com
 
-    ErrorLog ${APACHE_LOG_DIR}/nextcloud.error
-    CustomLog ${APACHE_LOG_DIR}/nextcloud.access combined
+        ErrorLog ${APACHE_LOG_DIR}/nextcloud.error
+        CustomLog ${APACHE_LOG_DIR}/nextcloud.access combined
 
-    <Directory /var/www/html/nextcloud/>
-        Require all granted
-        Options FollowSymlinks MultiViews
-        AllowOverride All
+        <Directory /var/www/html/nextcloud/>
+            Require all granted
+            Options FollowSymlinks MultiViews
+            AllowOverride All
 
-        <IfModule mod_dav.c>
-            Dav off
-        </IfModule>
+            <IfModule mod_dav.c>
+                Dav off
+            </IfModule>
 
-    SetEnv HOME /var/www/html/nextcloud
-    SetEnv HTTP_HOME /var/www/html/nextcloud
-    Satisfy Any
+        SetEnv HOME /var/www/html/nextcloud
+        SetEnv HTTP_HOME /var/www/html/nextcloud
+        Satisfy Any
 
-    </Directory>
+        </Directory>
 
-</VirtualHost>" >> /etc/apache2/sites-available/nextcloud.conf'
+    </VirtualHost>" >> /etc/apache2/sites-available/nextcloud.conf'
 
-sudo sh -c '
-echo "127.0.0.2       pcloud.com" >> /etc/hosts '
+    sudo sh -c '
+    echo "127.0.0.2       pcloud.com" >> /etc/hosts '
 
-sleep 1
-echo "Virtual host created"
-echo "Activating virtual host and setting up apache"
-sudo a2ensite nextcloud.conf
-sudo a2enmod rewrite headers env dir mime setenvif
-sudo systemctl restart apache2
+    sleep 5
+    echo "Virtual host created"
+    echo "Activating virtual host and setting up apache"
+    sudo a2ensite nextcloud.conf
+}
 
-sleep 1
+setapacheprem(){
+    echo -e "\n ${Cyan} Activating mods for Apache2.. ${Color_Off}"
+    sudo a2enmod rewrite headers env dir mime setenvif
+    sudo systemctl restart apache2
+} &> /dev/null
 
-echo "Setting up php memory limits..."
-sleep 1
 
-sudo sed -i 's/memory_limit = 128M/memory_limit = 612M/g' /etc/php/7.4/apache2/php.ini
-sudo rm -rf nextcloud-${Ver}
-echo "Nextcloud is up and running."
+phpmemlimit(){
+    echo -e "\n ${Cyan} Setting up php memory limits... ${Color_Off}"
+    sudo sed -i 's/memory_limit = 128M/memory_limit = 612M/g' /etc/php/7.4/apache2/php.ini
+}
+
+cleaning(){
+    echo -e "\n ${Cyan} Cleaning downloads... ${Color_Off}"
+    sudo rm -rf nextcloud-${Ver}
+    echo -e "\n ${Green} Nextcloud is installed."
+}
+
+
+#Run
+update
+installphpext
+getlatestnextcloud
+setPermissions
+#VirtualHost
+setapacheprem
+phpmemlimit
+cleaning
